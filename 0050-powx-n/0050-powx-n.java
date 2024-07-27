89653d4cf4f8e770a25aa263e8ed517086e3713d
AD4R5HG5 <117382591+100percentBAKA@users.noreply.github.com>
2024-07-27 22:53:33 +0530

    Time: 0 ms (100%), Space: 43.2 MB (6.13%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -9,17 +9,25 @@ class Solution {
             flag = true; // yes, n is negative
         }
 
-        while(copy > 0) {
-            if(copy % 2 == 0) {
-                x *= x;
-                copy /= 2;
-            }
-            else {
-                result *= x;
-                copy -= 1;
-            }
-        }
+        // while(copy > 0) {
+        //     if(copy % 2 == 0) {
+        //         x *= x;
+        //         copy /= 2;
+        //     }
+        //     else {
+        //         result *= x;
+        //         copy -= 1;
+        //     }
+        // }
+
+        result = pow(x, copy);
 
         return flag ? 1.0 / result : result;
     }
+
+    static double pow(double base, long power) {
+	    if(power == 0) return 1;
+	    if(power % 2 == 0) return pow(base * base, power / 2);
+	    return base * pow(base, power - 1);
+	}
 }
