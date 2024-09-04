a105ba8903aec424e3d024f08d7752f3cd2d03aa
AD4R5HG5 <117382591+100percentBAKA@users.noreply.github.com>
2024-09-04 11:58:58 +0530

    Time: 59 ms (12.41%), Space: 54.4 MB (6.39%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
@@ -0,0 +1,44 @@
+// class Solution {
+//     static int helper(int num) {
+//         // check if num is a perfect sq 
+//         int value = (int) Math.sqrt(num);
+//         return value * value == num ? value : -1;
+//     }
+
+//     public boolean judgeSquareSum(int c) {
+//         if(c == 0) return true;
+//         int i = 0, j = 0;
+
+//         while(i * i < c) {
+//             j = c - (i * i);
+//             if(helper(j) != -1) {
+//                 // is a perfect sq rt 
+//                 return true;
+//             }
+//             i++;
+//         }
+
+//         return false;
+//     }
+// }
+
+
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        if(c == 0) return true;
+
+        Set<Integer> arr = new HashSet<>();
+
+        for(int i = 0; i <= Math.sqrt(c); i++) {
+            // System.out.println("adding " + (i * i));
+            arr.add(i * i);
+        }
+
+        for(int i = 0; i < arr.size(); i++) {
+            // System.out.println(c - (i * i));
+            if(arr.contains(c - (i * i))) return true;
+        }
+
+        return false;
+    }
+}
