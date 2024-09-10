e1f1b45defb61d708c2002be12663a38830e2dfb
AD4R5HG5 <117382591+100percentBAKA@users.noreply.github.com>
2024-09-10 23:55:05 +0530

    Time: 1137 ms (29.43%), Space: 0B (100%) - LeetHub

--- 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
@@ -1,8 +1,20 @@
 -- select contest_id, round((count(user_id) / select count(user_id) as total from Users) * 100, 2) as percentage from Register group by contest_id;
 
 
-select contest_id, round(
-    ((count(user_id) / (select count(user_id) from Users)) * 100), 2
+-- select contest_id, round(
+--     ((count(user_id) / (select count(user_id) from Users)) * 100), 2
+-- ) as percentage 
+-- from Register group by contest_id
+-- order by percentage desc, contest_id asc;
+
+with s as (
+    select count(user_id) as total from Users
+)
+
+select r.contest_id, round(
+    ((count(r.user_id) / s.total) * 100), 2
 ) as percentage 
-from Register group by contest_id
+from Register r
+cross join s
+group by contest_id
 order by percentage desc, contest_id asc;
