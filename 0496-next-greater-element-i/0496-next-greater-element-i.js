53d1c19537bc72574c79d344e2974c97c203acab
AD4R5HG5 <117382591+100percentBAKA@users.noreply.github.com>
2024-09-26 15:31:55 +0530

    Time: 66 ms (33.98%), Space: 52.1 MB (44.99%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.js
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -5,30 +5,32 @@
  */
 
 var nextGreaterElement = function(nums1, nums2) {
-    const ref = []
+    const map = new Map()
 
     for(let i = 0; i < nums2.length; i++) {
-        let flag = true 
-        let j = i + 1;
+        let j = i + 1
+        let flag = true
 
         while(j < nums2.length) {
             if(nums2[i] < nums2[j]) {
-                ref[nums2[i]] = nums2[j]
-                flag = false
+                flag = false // no we have found the next larger element, no need to add -1
+                map.set(nums2[i], nums2[j])
                 break
             }
             j++
         }
 
         if(flag) {
-            ref[nums2[i]] = -1;
+            map.set(nums2[i], -1)
         }
     }
 
     const result = []
 
     for(let i = 0; i < nums1.length; i++) {
-        result.push(ref[nums1[i]])
+        result.push(
+            map.get(nums1[i])
+        )
     }
 
     return result
