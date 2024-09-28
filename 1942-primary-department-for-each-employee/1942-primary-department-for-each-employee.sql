b0134feefa2e957fc6886c741ac1e7bb195c959e
AD4R5HG5 <117382591+100percentBAKA@users.noreply.github.com>
2024-09-28 22:14:30 +0530

    Time: 486 ms (75.51%), Space: 0B (100%) - LeetHub

--- 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
@@ -1,18 +1,8 @@
-with s as (
-    select employee_id, department_id from Employee where primary_flag = 'Y'
-)
+select employee_id, department_id 
+from Employee 
+where primary_flag = 'Y' or employee_id in (
+    select employee_id from Employee group by employee_id having count(*) = 1
+);
 
-select 
-    e.employee_id, 
-    case 
-        when s.employee_id is not null then s.department_id 
-        else e.department_id
-    end as department_id
-from 
-    Employee e
-left join s
-    on s.employee_id = e.employee_id
-group by
-    e.employee_id, department_id
-order by
-    e.employee_id asc;
+
+-- select employee_id from Employee group by employee_id having count(*) = 1;
