a9372fa64bee8f8b0d040ead5463751f442badc4
AD4R5HG5 <117382591+100percentBAKA@users.noreply.github.com>
2025-02-07 12:35:17 +0530

    Time: 0 ms (100%), Space: 41.5 MB (39.39%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
@@ -1,37 +1,15 @@
 class Solution {
     public boolean check(int[] nums) {
-        int left = 0, right = 0;
-        boolean flag = false; // false --> no we have not found the point
+        int counter = 0;
 
-        for(int i = 1; i < nums.length; i++) {
-            if(nums[i - 1] > nums[i]) {
-                flag = true; // found the point
-                left = i - 1;
-                right = i;
-
-                break;
-            }
-        }
-
-        if(flag) {
-            // find the min of leftArr and max of rightArr
-            int min = nums[0];
-            for(int i = 1; i <= left; i++) {
-                if(nums[i - 1] > nums[i]) {
-                    return false;
-                }
-                min = Math.min(min, nums[i]);
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] > nums[(i + 1) % nums.length]) {
+                counter++;
             }
 
-            int max = nums[right]; 
-            for(int i = right + 1; i < nums.length; i++) {
-                if(nums[i - 1] > nums[i]) {
-                    return false;
-                }
-                max = Math.max(max, nums[i]);
+            if(counter > 1) {
+                return false;
             }
-
-            return (min < max) ? false : true;
         }
 
         return true;
